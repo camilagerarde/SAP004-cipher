9a591f82a2db785646b8d120680195afefafdf6f
camilagerarde <millagerarde@gmail.com>
2020-04-02 17:51:13 -0300

    Construção do código, estilo e estrutura.

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,26 @@
 const cipher = {
-  // ...
+  encode,
+  decode
 };
 
 export default cipher;
+
+function encode (offset, string) {
+  let z = "";
+  for (let i = 0; i < string.length; i++) {
+    let x = string.charCodeAt([i]);
+    let y = (((x - 65 + offset) % 26)+ 65);
+    z += (String.fromCharCode(y));
+  }
+  return z;
+}
+
+function decode (offset, string) {
+  let z = "";
+  for (let i = 0; i < string.length; i++) {
+    let x = string.charCodeAt([i]);
+    let y = (((x + 65 - offset) % 26)+ 65);
+    z += (String.fromCharCode(y));
+  }
+  return z;
+}
