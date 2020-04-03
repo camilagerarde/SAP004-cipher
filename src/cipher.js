9cabeaf98a19471ce8f86e96a346c32926c4ffca
camilagerarde <millagerarde@gmail.com>
2020-04-03 19:18:49 -0300

    Alterações para melhorar o código

--- src/cipher.js
+++ src/cipher.js
@@ -1,26 +1,45 @@
 const cipher = {
   encode,
-  decode
+  decode,
 };
 
-export default cipher;
-
-function encode (offset, string) {
-  let z = "";
-  for (let i = 0; i < string.length; i++) {
-    let x = string.charCodeAt([i]);
-    let y = (((x - 65 + offset) % 26)+ 65);
-    z += (String.fromCharCode(y));
+//Função de cifragem
+function encode (offset, message) {
+  if (typeof mensagem !== "string") {
+    throw new TypeError();
   }
-  return z;
+
+  let messageEncode = "";
+
+  for (let i = 0; i < message.length; i++) {
+    if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90){
+      let upperCaseEncode = ((message.charCodeAt(i) - 65 + offset) % 26)+ 65;
+      messageEncode += String.fromCharCode(upperCaseEncode);
+    } else if (message.charCodeAt(i) >= 97 && message.charCodeAt(i) <= 122) {
+      let lowerCaseEncode = ((message.charCodeAt(i) - 97 + offset) % 26)+ 97;
+      messageEncode += String.fromCharCode(lowerCaseEncode);
+    } else { 
+      messageEncode += String.fromCharCode(message.charCodeAt(i));
+    }
+  }
+  return messageEncode;
 }
 
-function decode (offset, string) {
-  let z = "";
+//Função de decifragem
+function decode (offset, message) {
+  if (typeof mensagem !== "string"){
+    throw new TypeError();
+  }
+
+  let messageDecode = "";
+
   for (let i = 0; i < string.length; i++) {
+    if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) //parei aqui
     let x = string.charCodeAt([i]);
     let y = (((x + 65 - offset) % 26)+ 65);
     z += (String.fromCharCode(y));
   }
   return z;
 }
+
+export default cipher;
