86a5442a314bdf09e568e75f46c1fd2f1dc61689
camilagerarde <millagerarde@gmail.com>
2020-04-07 12:19:46 -0300

    Criação do index, alterações no html e css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const encode = document.getElementById("encodeButton");
+const decode = document.getElementById("decodeButton");
+
+encode.addEventListener("click", clickencode);
+decode.addEventListener("click", clickdecode);
+
+function clickencode() {
+    event.preventDefault();
+    let message = document.getElementById("message").value;
+    let offset = Number(document.getElementById("key").value);
+    document.getElementById("finalMessage").innerHTML = cipher.encode(offset, message);
+} 
+
+function clickdecode() {
+    event.preventDefault();
+    let message = document.getElementById("message").value;
+    let offset = Number(document.getElementById("key").value);
+    document.getElementById("finalMessage").innerHTML = cipher.decode(offset, message);
+}
