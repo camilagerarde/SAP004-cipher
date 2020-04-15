d662ebe8151e682c97cfca4090638aee89a2d621
camilagerarde <millagerarde@gmail.com>
2020-04-15 19:06:04 -0300

    Código refatorado

--- src/cipher.js
+++ src/cipher.js
@@ -1,48 +1,36 @@
 const cipher = {
+  encodeOrDecode,
   encode,
   decode
 };
 
-function encode(offset, message) {
+function encodeOrDecode(offset, message, num) {
   if (typeof message !== "string") {
     throw new TypeError("message undefined");
   }
 
-  let messageEncode = "";
-
-  for (let i in message) {
-    if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90){
-      let upperCase = ((message.charCodeAt(i) - 65 + offset) % 26)+ 65;
-      messageEncode += String.fromCharCode(upperCase);
-    } else if (message.charCodeAt(i) >= 97 && message.charCodeAt(i) <= 122) {
-      let lowerCase = ((message.charCodeAt(i) - 97 + offset) % 26)+ 97;
-      messageEncode += String.fromCharCode(lowerCase);
-    } else { 
-      messageEncode += String.fromCharCode(message.charCodeAt(i));
-    }
-  }
-  return messageEncode;
-}
-
-function decode(offset, message) {
-  if (typeof message !== "string") {
-    throw new TypeError("message undefined");
-  }
-  
-  let messageDecode = "";
+  let finalMessage = "";
 
   for (let i in message) {
     if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
-      let upperCase = ((message.charCodeAt(i) - 90 - offset) % 26)+ 90;
-      messageDecode += String.fromCharCode(upperCase);
+      let upperCase = ((message.charCodeAt(i) - (65 + num) + offset) % 26) + 65 + num;
+      finalMessage += String.fromCharCode(upperCase);
     } else if (message.charCodeAt(i) >= 97 && message.charCodeAt(i) <= 122) {
-      let lowerCase = ((message.charCodeAt(i) - 122 - offset) %26)+ 122;
-      messageDecode += String.fromCharCode(lowerCase);
+      let lowerCase = ((message.charCodeAt(i) - (97 + num) + offset) % 26) + 97 + num;
+      finalMessage += String.fromCharCode(lowerCase);
     } else {
-      messageDecode += String.fromCharCode(message.charCodeAt(i));
+      finalMessage += String.fromCharCode(message.charCodeAt(i));
     }
   }
-  return messageDecode;
+  return finalMessage;
+}
+
+function encode(offset, message) {
+  return encodeOrDecode(offset, message, 0)
+}
+
+function decode(offset, message) {
+  return encodeOrDecode(-offset, message, 25)
 }
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import cipher from './cipher.js';
 
-const encode = document.getElementById("encodeButton").addEventListener("click", clickencode);
-const decode = document.getElementById("decodeButton").addEventListener("click", clickdecode);
+document.getElementById("encodeButton").addEventListener("click", clickencode);
+document.getElementById("decodeButton").addEventListener("click", clickdecode);
 
 function click(event, callback) {
-    event.preventDefault();
-    let message = document.getElementById("message").value;
-    let offset = Number(document.getElementById("key").value);
-    document.getElementById("finalMessage").innerHTML = callback(offset, message);
+  event.preventDefault();
+  let message = document.getElementById("message").value;
+  let offset = Number(document.getElementById("key").value);
+  document.getElementById("finalMessage").innerHTML = callback(offset, message);
 }
+
 function clickencode() {
-    click(event, cipher.encode);
-} 
+  click(event, cipher.encode);
+}
+
 function clickdecode() {
-    click(event, cipher.decode);
+  click(event, cipher.decode);
 }
